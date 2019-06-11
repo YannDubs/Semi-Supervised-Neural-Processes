823423821f1cbfd1f0aceab812c17e8f80a0534f
Y. Dubois <yd292@mlsalt-cpu2.eng.cam.ac.uk>
2019-06-11 19:28:40 +0100

    Remove dirt

--- notebooks/ntbks_datasets.py
+++ notebooks/ntbks_datasets.py
@@ -71,6 +71,10 @@ class GPDataset(Dataset):
             self.precompute_data()
         return self.data[self.counter], self.targets[self.counter]
 
+    def extrapolation_batch(self, ):
+        """Return a batch of extrapolation"""
+        pass
+
     def __len__(self):
         return self.num_samples
 
@@ -146,25 +150,3 @@ class SineDataset(Dataset):
 
     def __len__(self):
         return self.num_samples
-
-
-m
-datatasets = dict()
-kwargs = dict(num_samples=num_samples, num_points=num_points)
-datatasets["rbf"] = GPDataset(kernel=1.0 * RBF(length_scale=1.0,
-                                               length_scale_bounds=(1e-1, 10.0)),
-                              **kwargs)
-
-datatasets["periodic"] = GPDataset(kernel=1.0 * ExpSineSquared(length_scale=1.0,
-                                                               periodicity=3.0,
-                                                               length_scale_bounds=(0.1, 10.0),
-                                                               periodicity_bounds=(1.0, 10.0)),
-                                   **kwargs)
-datatasets["non-stationary"] = GPDataset(kernel=(ConstantKernel(0.1, (0.01, 10.0)) *
-                                                 (DotProduct(sigma_0=1.0,
-                                                             sigma_0_bounds=(0.1, 10.0)) ** 2)),
-                                         **kwargs)
-datatasets["matern"] = GPDataset(kernel=1.0 * Matern(length_scale=1.0,
-                                                     length_scale_bounds=(1e-1, 10.0),
-                                                     nu=1.5),
-                                 **kwargs)
